1f12bde39217f026383f42fe593affa905f8073f
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-04-28 20:53:19 -0300

    Utilizando JS no HTML

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+/*
+"user strict";
+//isso evita deixa variveis soltas
+
+
+/*console.log("Olá Mundo!");*/
+
+/*var nr1 = "10";
+var nr2 = "5";
+
+//Realizando operações com Strings;
+//tem que usar função de parciamento
+
+console.log(parseInt(nr1) + parseInt(nr2));
+
+var nr3 */
+
+
+var nome1 = "Dubas";
+let nome2 = "Serbatinho";
+const nome3 = "Julia";
+//variavelo const não se altera durante o fluxo
+
+if(nome1 != ""){
+    let nome1 ="Julia";
+}
+
+console.log(nome1);
