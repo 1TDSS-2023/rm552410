e79b851afaf525dafc132656f3aca6d7075c0785
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-05-05 20:00:52 -0300

    Array

--- js/script.js
+++ js/script.js
@@ -33,35 +33,35 @@ console.log(nome1);*/
 //banana na primeira posição mas no indice 0
 let frutas = ["banana", "maça", "laranja", "morango", "uva", "amora", "cereja", "abacaxi"];
 
-console.log(frutas)
-console.log(frutas[7])
+//console.log(frutas)
+//console.log(frutas[7])
 
 //verificando o tamanho do array através da propriedade length.
-console.log(`Tamanho do Array é:  ${frutas.length}`);
+//console.log(`Tamanho do Array é:  ${frutas.length}`);
 
 
 //Inserindo um item ao final do Array com o método push(nomeItem)
-frutas.push("pêssego");
-console.log(frutas)
+//frutas.push("pêssego");
+//console.log(frutas)
 
 //Inserindo um item no inicio do Array com o método unshift(nomeItem)
-frutas.unshift("Kiwi");
-console.log(frutas)
+//frutas.unshift("Kiwi");
+//console.log(frutas)
 
 //Removendo um item ao final do Array com o método pop(), não precisa passar um parâmetro
-frutas.pop();
-console.log(frutas)
+//frutas.pop();
+//console.log(frutas)
 
 //Removendo um item no inicio do Array com o método shift();
-frutas.shift();
-console.log(frutas);
+//frutas.shift();
+//console.log(frutas);
 
 
 //Localizando um item no Array com o método indexOf(nomeitem);
 //Obs: O indexOf (nomeItem) retorna o indice do item.
 //let indice = frutas.indexOf("uva");
-console.log(`Indice do item buscado: ${indice}`);
-console.log(`Elemento buscado: ${frutas[indice]}`)
+//console.log(`Indice do item buscado: ${indice}`);
+//console.log(`Elemento buscado: ${frutas[indice]}`)
 
 
 //Removendo um item do Array com o método splice(indice do item, qtd de vezes que 
@@ -69,8 +69,17 @@ console.log(`Elemento buscado: ${frutas[indice]}`)
 //obs: utilize o método indexOf(nomeItem) para descobrir o indice do item. 
 
 
-let indice  = frutas.indexOf("amora");
-console.log(`Elemento no índice antes da remoção: ${frutas[index]}`);
-frutas. splice(indice,1);
+/*let indice  = frutas.indexOf("amora");
+console.log(`Elemento no índice antes da remoção: ${frutas[indice]}`);
+frutas. splice(indice, 1);
 console.log(frutas);
 console.log(`Elemento no índice após a remoção: ${frutas[indice]}`);
+
+//Fazendo tabela
+console.table(frutas)*/
+
+
+frutas.forEach((fruta) => {
+    console.log("FRUTA DA VEZ: " + fruta)
+
+});
